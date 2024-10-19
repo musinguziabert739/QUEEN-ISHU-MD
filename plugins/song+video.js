2d259fed285a3c6a0aef1bd44b6f334ae4020d1c
laksidunimsara1 <laksidunimsara200803041@gmail.com>
2024-10-19 07:22:30 +0530

    Create song+video.js

---
+++ plugins/song+video.js
@@ -0,0 +1,37 @@
+const {cmd , commands} = require('../command')
+const fg = require('api-dylux')
+const yts = require('yt-search')
+
+
+cmd({
+    pattern: "song",
+    desc: "download song",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply("please give me url")
+const search = await yts(q)
+const data = search.videos[0];
+const url = data.url 
+
+let desc = 'laksidu downlod'
+
+await comn .sendMessage(frome,{image:{url: data.thumbnail},caption:desc},{quoted:mek});
+  
+///download audio 
+
+let down = await fg.yta(url)
+let downloadUrl = down.dl_url
+
+//send audio message
+await conn.sendMessage(frome,{audio: {url:downloadUrl},mimetype:"audio/mpeg"},{quoted:mek})
+
+  
+ 
+}catch(e){
+console.log(e)
+reply('${e}')
+}
+})
