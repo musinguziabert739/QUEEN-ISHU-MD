9d25377f9ce4f3f9235236105423e1a7f90aa5b9
laksidunimsara1 <laksidunimsara200803041@gmail.com>
2024-10-19 14:18:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,9 +7,4 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-MODE: process.env.MODE || "public",
-AUTO_VOICE: process.env.AUTO_VOICE || "true",
-AUTO_STICKER: process.env.AUTO_STICKER || "true",
-AUTO_REPLY: process.env.AUTO_REPLY || "true",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 };
