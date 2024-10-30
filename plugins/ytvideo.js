94e4e328487f49c35ab8601a423f9d0ff90737e9
LAKSIDU_NIMSARA <laksidunimsara200803041@gmail.com>
2024-10-30 19:04:45 +0530

    Update ytvideo.js

--- plugins/ytvideo.js
+++ plugins/ytvideo.js
@@ -1,1 +1,49 @@
-const _0x40e31b=_0x19fd;function _0x19fd(_0x47215f,_0x4178c3){const _0x139ec6=_0x139e();return _0x19fd=function(_0x19fd93,_0x257868){_0x19fd93=_0x19fd93-0x171;let _0x4a3dd6=_0x139ec6[_0x19fd93];return _0x4a3dd6;},_0x19fd(_0x47215f,_0x4178c3);}(function(_0x4aad7f,_0x839542){const _0x43471e=_0x19fd,_0x23b138=_0x4aad7f();while(!![]){try{const _0x185963=-parseInt(_0x43471e(0x188))/0x1*(-parseInt(_0x43471e(0x185))/0x2)+parseInt(_0x43471e(0x191))/0x3+parseInt(_0x43471e(0x18d))/0x4+-parseInt(_0x43471e(0x18a))/0x5*(parseInt(_0x43471e(0x192))/0x6)+-parseInt(_0x43471e(0x179))/0x7*(parseInt(_0x43471e(0x189))/0x8)+-parseInt(_0x43471e(0x195))/0x9+-parseInt(_0x43471e(0x174))/0xa;if(_0x185963===_0x839542)break;else _0x23b138['push'](_0x23b138['shift']());}catch(_0x2af3fc){_0x23b138['push'](_0x23b138['shift']());}}}(_0x139e,0xbe9f8));const {cmd}=require(_0x40e31b(0x17a)),{fetchJson}=require('../lib/functions'),apilink=_0x40e31b(0x193);function _0x139e(){const _0x44be29=['download','607719CAUICo','../command','-\x20*Error*','video/mp4','title','data','download\x20videos.','Give\x20me\x20song\x20name\x20or\x20url\x20!','/search/yt?q=','author','url','.mp4','6428usKodo','\x0a\x0a💛\x20*Ago\x20:*\x20','result','359VtyhRp','56rbUxbp','1039095aNUwBa','views','video','5940716pXFDbM','name','key','\x0a\x0a💛\x20*Views\x20:*\x20','3928275mnqGuz','18ccRALO','https://dark-yasiya-api-new.vercel.app','\x0a╰━========∙━\x20─┉─\x20•\x20=====⊷\x0a\x0a>\x20*©\x20CREATED\x20BY\x20LAKSIDU\x20NIMSARA\x20·\x20·\x20·*\x20💛\x0a>\x20QUUEN-ISHU-MD\x0a','5854734xreZAm','sendMessage','dl_link','\x0a\x0a💛\x20*Duration\x20:*\x20','12859360ZuNCFu','\x0a\x0a💛\x20*URL\x20:*\x20','ago','log'];_0x139e=function(){return _0x44be29;};return _0x139e();}cmd({'pattern':_0x40e31b(0x18c),'desc':_0x40e31b(0x17f),'category':_0x40e31b(0x178),'react':'💛','filename':__filename},async(_0x43a331,_0x1e1dec,_0x26cf86,{from:_0x429b46,reply:_0x436989,q:_0x3be07f})=>{const _0x1ff5b5=_0x40e31b;try{if(!_0x3be07f)return _0x436989(_0x1ff5b5(0x180));const _0x14cc59=await fetchJson(apilink+_0x1ff5b5(0x181)+_0x3be07f),_0x378514=_0x14cc59[_0x1ff5b5(0x187)][_0x1ff5b5(0x17e)][0x0],_0x31a202=_0x378514[_0x1ff5b5(0x183)],_0x1e7ab0=await fetchJson(apilink+'/download/mp4?url='+_0x378514[_0x1ff5b5(0x183)]);let _0xceb7b1='‎‎\x0a*===============*\x0a_*💛\x20SONG\x20DOWNLOADER\x20💛*_\x0a*================*\x0a╭━=======━\x20─┉─\x20•\x20─┉─⊷\x0a💛\x20*Title\x20:*\x20'+_0x378514[_0x1ff5b5(0x17d)]+'\x0a\x0a💛\x20*Author\x20:*\x20'+_0x378514[_0x1ff5b5(0x182)][_0x1ff5b5(0x18e)]+_0x1ff5b5(0x173)+_0x378514['timestamp']+_0x1ff5b5(0x186)+_0x378514[_0x1ff5b5(0x176)]+_0x1ff5b5(0x190)+_0x378514[_0x1ff5b5(0x18b)]+_0x1ff5b5(0x175)+_0x378514[_0x1ff5b5(0x183)]+_0x1ff5b5(0x194);const _0x94c41c=await _0x43a331['sendMessage'](_0x429b46,{'image':{'url':_0x378514['thumbnail']},'caption':_0xceb7b1},{'quoted':_0x1e1dec});await _0x43a331[_0x1ff5b5(0x171)](_0x429b46,{'video':{'url':_0x1e7ab0[_0x1ff5b5(0x187)][_0x1ff5b5(0x172)]},'mimetype':_0x1ff5b5(0x17c)},{'quoted':_0x1e1dec}),await _0x43a331[_0x1ff5b5(0x171)](_0x429b46,{'document':{'url':_0x1e7ab0[_0x1ff5b5(0x187)]['dl_link']},'mimetype':_0x1ff5b5(0x17c),'fileName':_0x378514[_0x1ff5b5(0x17d)]+_0x1ff5b5(0x184),'caption':'*©\x20CREATED\x20BY\x20LAKSIDU\x20NIMSARA\x20·\x20·\x20·*\x20💛'},{'quoted':_0x1e1dec}),await _0x43a331[_0x1ff5b5(0x171)](_0x429b46,{'delete':_0x94c41c[_0x1ff5b5(0x18f)]});}catch(_0x3ddb80){console[_0x1ff5b5(0x177)](_0x3ddb80),_0x436989(_0x1ff5b5(0x17b));}});
+// YT MP3 DOWNLOAD COMMAND 
+
+const { cmd } = require('../command')
+const { fetchJson } = require('../lib/functions')
+
+const apilink = 'https://dark-yasiya-api-new.vercel.app' // API LINK ( DO NOT CHANGE THIS!! )
+
+cmd({
+    pattern: "video",
+    desc: "download video.",
+    category: "download",
+    react: "🎧",
+    filename: __filename
+},
+async(conn, mek, m,{from, reply, q}) => {
+try{
+
+if(!q) return reply('Give me song name or url !')
+    
+const search = await fetchJson(`${apilink}/search/yt?q=${q}`)
+const data = search.result.data[0];
+const url = data.url
+    
+const ytdl = await fetchJson(`${apilink}/download/ytmp4?url=${data.url}`)
+    
+let message = `‎‎           🎶 YT SONG DOWNLOADER 🎶
+
+
+ 🎵 ‎Title: ${data.title}
+ ⏱ Duration: ${data.timestamp}
+ 🌏 Uploaded: ${data.ago}
+ 🧿 Views: ${data.views}
+ 🤵 Author: ${data.author.name}
+  📎 Url: ${data.url}
+`
+  
+await conn.sendMessage(from, { image: { url : data.thumbnail }, caption: message }, { quoted : mek })
+  
+// SEND AUDIO NORMAL TYPE and DOCUMENT TYPE
+await conn.sendMessage(from, { video: { url: ytdl.result.dl_link }, mimetype: "video/mpeg" }, { quoted: mek })
+await conn.sendMessage(from, { document: { url: ytdl.result.dl_link }, mimetype: "video/mpeg", fileName: data.title + ".mp3", caption: `${data.title}`}, { quoted: mek })
+  
+} catch(e){
+console.log(e)
+reply(e)
+}
+})
+
+// FOLLOW US : https://whatsapp.com/channel/0029VaaPfFK7Noa8nI8zGg27
