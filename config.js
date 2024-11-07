96d5d6e827434c79f4fa75a21b18bbf7b9da54df
LAKSIDU_NIMSARA <laksidunimsara200803041@gmail.com>
2024-11-07 20:09:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "LMBUCBrI#df19bwdsD4EzLdd4HDYLBvaQbavWBlso47V8z3i7EWg",
+SESSION_ID: process.env.SESSION_ID || "",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
