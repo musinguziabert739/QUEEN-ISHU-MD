13ce4a9bd70a8d65075f5509e3b58e51db30b011
LAKSIDU_NIMSARA <laksidunimsara200803041@gmail.com>
2024-11-24 16:06:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -13,28 +13,7 @@ AUTO_STICKER: process.env.AUTO_STICKER || "true",
 AUTO_REPLY: process.env.AUTO_REPLY || "true",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.imgur.com/RQOPG85.jpeg",
-ALIVE_MSG: process.env.ALIVE_MSG || "╒✦•=============•••••••••============•✦
-│ CREATER : LAKSIDU NIMSARA
-│ VERSION : ᴠ.2.5.0
-│ UPTIME  :  1 hour, 28 minutes, 6 seconds
-│ ʀᴀᴍ ᴜꜱᴀɢᴇ  : 48.07MB / 63276MB
-│ HOST NAME : 64ce5d1c-2f9d-4f90-bd6e-3d8e6965a3f9
-╘✦•================•••••••••===============•✦
-────────────────
-💜𝐐𝐔𝐄𝐄𝐍 𝐈𝐒𝐇𝐔 𝐀𝐋𝐈𝐕𝐄 𝐍𝐎𝐖💜
-────────────────
-╭──────────────
-│ 💜 වික්ශිප්ත ආකාසත් කියනවා විටෙක දුක්බර කතාවක්🌥️
-│ 💜 කදුලු ලෙස වැටෙනවා නොව්ව් ද එහි විඩාවත්🌧️
-│ 💜 දාරාන තවමත් ප්‍රේම කරනවා නොවෙනස්  සූරියකාන්තත්🌻
-│ 💜 කොච්චර රිදුනත් බලාන ඉන්නවා තවටත් සූර්යයා දිහාත් 🌞
-│ 💜 පතොකුත් විටෙකදී ඉල්ලනවා නොවෙද සැනසිමට වැහි බින්දුත් 🌵
-│ 💜 වැස්සත් දන්නවා බලාන ඉන්නවමයි කියන්න පතොකුත් 🌧️🌵
-│ 💜 හරියට අපි වගේමයි ස්වභාව ධර්මයත් 😊
-│ 💜 ආදරයෙන් බලාන ඉන්න හැමෝම දවසක එකතු වෙන්න ඕනි ඒක මගෙ පුන්චි ප්‍රාර්ථනාවක්හරි ආදරයෙන් 💐❤️
-│ 💜 ස්තූතිය....!
-⁠⁠⁠⁠╰─────────────
-© CREATED BY LAKSIDU NIMSARA · · ·",
+ALIVE_MSG: process.env.ALIVE_MSG || "╒✦•=============•••••••••============•✦│ CREATER : LAKSIDU NIMSARA│ VERSION : ᴠ.2.5.0│ ╘✦•==============•••••••••===========•✦",
 ANTI_LINK: process.env.ANTI_LINK || "true",
 ANTI_BAD: process.env.ANTI_BAD || "true",
 PREFIX: process.env.PREFIX || ".",
