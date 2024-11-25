1dee68fac7fcd46ec5a762678e0e6d637d1dfe62
LAKSIDU_NIMSARA <laksidunimsara200803041@gmail.com>
2024-11-25 21:05:08 +0530

    Update ai.js

--- plugins/ai.js
+++ plugins/ai.js
@@ -1,7 +1,6 @@
 const config = require('../config')
 const {cmd , commands} = require('../command')
 const { fetchJson } = require('../lib/functions')
-//============================================================ai chat gpt===========================================================
 cmd({
     pattern: "ai",
     alias: ["gpt","bot"], 
