8069531392d278cea875f6f52e1a3339ff1dc18c
LAKSIDU_NIMSARA <laksidunimsara200803041@gmail.com>
2024-11-25 16:46:53 +0530

    舞~ 🎀 𝒬𝒰𝐸𝐸𝒩 𝐼𝒮𝐻𝒰 🎀 ~舞

--- plugins/ai.js
+++ plugins/ai.js
@@ -1,7 +1,7 @@
 const config = require('../config')
 const {cmd , commands} = require('../command')
 const { fetchJson } = require('../lib/functions')
-
+//============================================================ai chat gpt===========================================================
 cmd({
     pattern: "ai",
     alias: ["gpt","bot"], 
