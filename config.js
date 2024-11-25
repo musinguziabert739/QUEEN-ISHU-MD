4d0c8f8ef51930e56493d037dcfc5ae15f485dc5
LAKSIDU_NIMSARA <laksidunimsara200803041@gmail.com>
2024-11-25 10:10:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,23 +7,23 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 //==========================================- MAIN - CONFIGS -==================================================================
 SESSION_ID: process.env.SESSION_ID || "enter your session",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-MODE: process.env.MODE || "public",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "",
+MODE: process.env.MODE || "",
 PREFIX: process.env.PREFIX || ".",
 OWNER_REACT: process.env.OWNER_REACT || "true",
-BOT_NAME: process.env.BOT_NAME || "➺ѕเℓεɳƭ_ℓσѵε૨࿐",
+BOT_NAME: process.env.BOT_NAME || "",
   //========================================- OTHER - CONFIGS -=====================================================================
-AUTO_VOICE: process.env.AUTO_VOICE || "true",
-AUTO_STICKER: process.env.AUTO_STICKER || "true",
-AUTO_REPLY: process.env.AUTO_REPLY || "true",
-ANTI_LINK: process.env.ANTI_LINK || "true",
-ANTI_BAD: process.env.ANTI_BAD || "true",
-FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
-AUTO_REACT: process.env.AUTO_REACT || "true",
+AUTO_VOICE: process.env.AUTO_VOICE || "",
+AUTO_STICKER: process.env.AUTO_STICKER || "",
+AUTO_REPLY: process.env.AUTO_REPLY || "",
+ANTI_LINK: process.env.ANTI_LINK || "",
+ANTI_BAD: process.env.ANTI_BAD || "",
+FAKE_RECORDING: process.env.FAKE_RECORDING || "",
+AUTO_REACT: process.env.AUTO_REACT || "",
 //==========================================- API-CONFIGS -==========================================================
 GOOGLE_CX: process.env.GOOGLE_CX || "AIzaSyD93IeJsouK51zjKgyHAwBIAlqr-a8mnME", 
 PASTEBIN_API_KEY: process.env.PASTEBIN_API_KEY || "uh8QvO6vQJGtIug9WvjdTAPx_ZAFJAxn",
 //------------------------------------------------------------------------------------------------------------------
-ALIVE_IMG: process.env.ALIVE_IMG || "https://i.imgur.com/RQOPG85.jpeg",
-ALIVE_MSG: process.env.ALIVE_MSG || "HII DEAR I AM ALIVE NOW ,",
+ALIVE_IMG: process.env.ALIVE_IMG || "",
+ALIVE_MSG: process.env.ALIVE_MSG || "",
 };
