abeee22bffaaccd4447a188ede2e3bea7dcb037d
LAKSIDU_NIMSARA <laksidunimsara200803041@gmail.com>
2024-11-25 13:51:14 +0530

    舞~   🎀  𝒬𝒰𝐸𝐸𝒩 𝐼𝒮𝐻𝒰  🎀   ~舞

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-//==========================================- MAIN - CONFIGS -==================================================================
+//==========================================- MAIN - CONFIGS -======================================================================
 SESSION_ID: process.env.SESSION_ID || "enter your session",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "",
 MODE: process.env.MODE || "",
@@ -20,10 +20,11 @@ ANTI_LINK: process.env.ANTI_LINK || "",
 ANTI_BAD: process.env.ANTI_BAD || "",
 FAKE_RECORDING: process.env.FAKE_RECORDING || "",
 AUTO_REACT: process.env.AUTO_REACT || "",
-//==========================================- API-CONFIGS -==========================================================
+//==========================================- API-CONFIGS -=========================================================================
 GOOGLE_CX: process.env.GOOGLE_CX || "AIzaSyD93IeJsouK51zjKgyHAwBIAlqr-a8mnME", 
 PASTEBIN_API_KEY: process.env.PASTEBIN_API_KEY || "uh8QvO6vQJGtIug9WvjdTAPx_ZAFJAxn",
-//------------------------------------------------------------------------------------------------------------------
+//-------------------------------------------ALIVE MSG-CONFIG-----------------------------------------------------------------------
 ALIVE_IMG: process.env.ALIVE_IMG || "",
 ALIVE_MSG: process.env.ALIVE_MSG || "",
+//---------------------------------------------------------------------------------------------------------------------------------
 };
