c18363171c1344d7ab99adc993f419fa9b26c019
LAKSIDU_NIMSARA <laksidunimsara200803041@gmail.com>
2024-11-26 09:37:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,24 +6,24 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 ///================================================MAIN-CONFIG============================================
-SESSION_ID: process.env.SESSION_ID || "CJ5gBbaC#vJzAbzPjfHzGv8-7qwrO0RZXevYOfDHeF036cVlr_Ac",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-MODE: process.env.MODE || "public",
-PREFIX: process.env.PREFIX || ".",
-OWNER_REACT: process.env.OWNER_REACT || "true",
+SESSION_ID: process.env.SESSION_ID || "CJ5gBbaC#vJzAbzPjfHzGv8-7qwrO0RZXevYOfDHeF036cVlr_Ac",  // ADD Your Session Id 
+MODE: process.env.MODE || "public",  // Add Your Bot Mode 
+PREFIX: process.env.PREFIX || ".",   // Add Your Custom Prefix 
+OWNER_REACT: process.env.OWNER_REACT || "true", //add true or false
 BOT_NAME: process.env.BOT_NAME || "laksidu",
 ///==========================================CUSTOM-SETTING-CONFIG=========================================
-AUTO_VOICE: process.env.AUTO_VOICE || "true",
-AUTO_STICKER: process.env.AUTO_STICKER || "true",
-AUTO_REPLY: process.env.AUTO_REPLY || "true",
-ANTI_LINK: process.env.ANTI_LINK || "true",
-ANTI_BAD: process.env.ANTI_BAD || "true",
-FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
-AUTO_REACT: process.env.AUTO_REACT || "true",
+AUTO_VOICE: process.env.AUTO_VOICE || "true", //add true or false
+AUTO_STICKER: process.env.AUTO_STICKER || "true", //add true or false
+AUTO_REPLY: process.env.AUTO_REPLY || "true", //add true or false
+ANTI_LINK: process.env.ANTI_LINK || "true", //add true or false
+ANTI_BAD: process.env.ANTI_BAD || "true", //add true or false
+FAKE_RECORDING: process.env.FAKE_RECORDING || "true", //add true or false
+AUTO_REACT: process.env.AUTO_REACT || "true", //add true or false
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", //add true or false
 ///===============================================API-CONFIG==============================================
 GOOGLE_CX: process.env.GOOGLE_CX || "AIzaSyD93IeJsouK51zjKgyHAwBIAlqr-a8mnME", 
 PASTEBIN_API_KEY: process.env.PASTEBIN_API_KEY || "uh8QvO6vQJGtIug9WvjdTAPx_ZAFJAxn",
 ///===============================================ALIVE-CONFIG=========================================
-ALIVE_IMG: process.env.ALIVE_IMG || "https://i.imgur.com/RQOPG85.jpeg",
-ALIVE_MSG: process.env.ALIVE_MSG || "HI AME WHATSAPP BOT",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.imgur.com/RQOPG85.jpeg", //add alive img url
+ALIVE_MSG: process.env.ALIVE_MSG || "HI AME WHATSAPP BOT", //add alive msg
 };
