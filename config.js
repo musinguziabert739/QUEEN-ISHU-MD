fd98670c16ee242d7bfc36c3a57788c7422f09f7
LAKSIDU_NIMSARA <laksidunimsara200803041@gmail.com>
2024-11-26 09:33:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,12 +5,14 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
+///================================================MAIN-CONFIG============================================
 SESSION_ID: process.env.SESSION_ID || "CJ5gBbaC#vJzAbzPjfHzGv8-7qwrO0RZXevYOfDHeF036cVlr_Ac",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-MODE: process.env.MODE || "true",
+MODE: process.env.MODE || "public",
 PREFIX: process.env.PREFIX || ".",
 OWNER_REACT: process.env.OWNER_REACT || "true",
 BOT_NAME: process.env.BOT_NAME || "laksidu",
+///==========================================CUSTOM-SETTING-CONFIG=========================================
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
 AUTO_STICKER: process.env.AUTO_STICKER || "true",
 AUTO_REPLY: process.env.AUTO_REPLY || "true",
@@ -18,8 +20,10 @@ ANTI_LINK: process.env.ANTI_LINK || "true",
 ANTI_BAD: process.env.ANTI_BAD || "true",
 FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
 AUTO_REACT: process.env.AUTO_REACT || "true",
+///===============================================API-CONFIG==============================================
 GOOGLE_CX: process.env.GOOGLE_CX || "AIzaSyD93IeJsouK51zjKgyHAwBIAlqr-a8mnME", 
 PASTEBIN_API_KEY: process.env.PASTEBIN_API_KEY || "uh8QvO6vQJGtIug9WvjdTAPx_ZAFJAxn",
+///===============================================ALIVE-CONFIG=========================================
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.imgur.com/RQOPG85.jpeg",
 ALIVE_MSG: process.env.ALIVE_MSG || "HI AME WHATSAPP BOT",
 };
