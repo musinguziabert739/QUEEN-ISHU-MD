fb9a9a8fb52bf762cbbda3f5c6a86c4f5e98b190
LAKSIDU_NIMSARA <laksidunimsara200803041@gmail.com>
2024-11-28 00:17:00 +0530

    舞~   🎀  𝒬𝒰𝐸𝐸𝒩 𝐼𝒮𝐻𝒰  🎀   ~舞

--- plugins/yts.js
+++ plugins/yts.js
@@ -7,7 +7,7 @@ const fs = require('fs-extra')
 var videotime = 60000 // 1000 min
 const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson} = require('../lib/functions')
 cmd({
-    pattern: "yts",
+    pattern: "yt",
     alias: ["ytsearch"],
     use: '.yts sameer kutti',
     react: "🔎",
