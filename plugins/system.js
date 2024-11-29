076c53fdf2ef0e49390b02f436f67df094c1d0c1
LAKSIDU_NIMSARA <laksidunimsara200803041@gmail.com>
2024-11-30 00:50:36 +0530

    舞~   🎀  𝒬𝒰𝐸𝐸𝒩 𝐼𝒮𝐻𝒰  🎀   ~舞

--- plugins/system.js
+++ plugins/system.js
@@ -23,7 +23,7 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 let status = `*╔══════✮❁•°♛°•❁✮ ══════╗*
-*SILENT-SOBX-MD UPTIME LIST↷*
+*QUEEN-ISHU-MD-SYSTERM*
 
 *_UPTIME:➠_*  ${runtime(process.uptime())}
 
