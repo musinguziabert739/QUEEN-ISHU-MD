c341ee64b7a4b16c9774e6322f0c6d10674178a6
LAKSIDU_NIMSARA <laksidunimsara200803041@gmail.com>
2024-12-01 00:26:04 +0530

    舞~   🎀  𝒬𝒰𝐸𝐸𝒩 𝐼𝒮𝐻𝒰  🎀   ~舞

--- plugins/ai.js
+++ plugins/ai.js
@@ -8,9 +8,11 @@ created by laksidu
  DONT COPY
 */
 
+
 const config = require('../config')
 const {cmd , commands} = require('../command')
 const { fetchJson } = require('../lib/functions')
+
 cmd({
     pattern: "ai",
     alias: ["gpt","bot"], 
@@ -22,15 +24,9 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 let data = await fetchJson(`https://chatgptforprabath-md.vercel.app/api/gptv1?q=${q}`)
-return reply(`${data.data}
-
-> QUEEN ISHU MD
-
-*CREATE BY LAKSIDU NIMSARA*`)
+return reply(`${data.data}`)
 }catch(e){
 console.log(e)
 reply(`${e}`)
 }
 })
-
-//===============================
